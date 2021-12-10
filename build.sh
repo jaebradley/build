998ca0649a6cf8b715318e32b5d25897fd7e6d4d
Jae Bradley <jae.b.bradley@gmail.com>
2021-12-10 13:47:12 -0800

    Import fail script in build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 . "$(dirname ${BASH_SOURCE})/utilities/applications/install_application_from_disk_image.sh"
+. "$(dirname ${BASH_SOURCE})/utilities/fail.sh"
 
 install_application_from_disk_image "https://github.com/rxhanson/Rectangle/releases/download/v0.49/Rectangle0.49.dmg" "Rectangle.app"
 if [[ $? -ne 0 ]]; then fail "Failed to install Rectangle.app"; fi
