777daedd4d4770a9dd85902f449255afd89c1ecc
Jae Bradley <jae.b.bradley@gmail.com>
2021-12-10 13:12:29 -0800

    Update

--- tests/start.sh
+++ tests/start.sh
@@ -2,4 +2,5 @@
 
 ls -la /Applications/
 
-if [[ -d "/Applications/Spotify.app" ]]; then echo "Spotify application does not exist" && exit 255; fi
+if [[ ! -d "/Applications/Spotify.app" ]]; then echo "Spotify application does not exist" && exit 255; fi
+if [[ ! -x "/Applications/Spotify.app" ]]; then echo "Spotify application is not executable" && exit 255; fi
