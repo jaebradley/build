92054c44a4304da4f8dffc8dd687af0621874250
Jae Bradley <jae.b.bradley@gmail.com>
2022-02-09 21:50:29 -0500

    Install meld application (#10)

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ disk_image_urls=(
   "https://download.jetbrains.com/idea/ideaIC-2021.2.3-aarch64.dmg?_gl=1*1oiw9gx*_ga*MTE1Mjg2MDU0OS4xNjM3MTIwNzM5*_ga_V0XZL7QHEB*MTYzNzEyMDc2Mi4xLjEuMTYzNzEyMDk0MS4w&_ga=2.235293319.74824018.1637120739-1152860549.1637120739"
   "https://download.jetbrains.com/python/pycharm-community-2021.3-aarch64.dmg?_gl=1*15mf13t*_ga*MTM3MDUzODQ0NS4xNjM5NjAwOTMy*_ga_V0XZL7QHEB*MTYzOTYwMDkzMi4xLjEuMTYzOTYwMDkzNi41Ng..&_ga=2.139141491.1583443186.1639600933-1370538445.1639600932"
   "https://download-installer.cdn.mozilla.net/pub/firefox/releases/96.0/mac/en-US/Firefox%2096.0.dmg"
+  "https://github.com/yousseb/meld/releases/download/osx-18/meldmerge.dmg"
 )
 
 application_names=(
@@ -18,6 +19,7 @@ application_names=(
   "Intellij IDEA CE.app"
   "Pycharm CE.app"
   "Firefox.app"
+  "Meld.app"
 )
 
 if [[ ${#disk_image_urls[@]} -ne ${#application_names[@]} ]]; then fail "Error on line ${LINENO}"; fi
