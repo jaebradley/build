91215e5a5c95809921d0dac9568867cfd018580e
Jae Bradley <jae.b.bradley@gmail.com>
2022-03-26 13:07:00 -0400

    Remove file check

--- applications/bash/configuration/bashrc/configure.sh
+++ applications/bash/configuration/bashrc/configure.sh
@@ -8,11 +8,6 @@ location_path="$HOME/.bashrc"
 directory_path="$(dirname "${location_path}")"
 file_name="$(basename "${location_path}")"
 
-if [[ -f "${location_path}" ]]
-then
-  chmod +x "${location_path}" || fail "Unable to add execute permissions on file ${location_path}"
-fi
-
 mkdir -p "${directory_path}" || fail "Failed to create directory ${directory_path}"
 cp -n "${content_path}" "${location_path}" || fail "Failed to copy content from ${content_path} to ${location_path}"
 chmod 755 "${location_path}" || fail "Unable to set permissions for "${location_path}""
