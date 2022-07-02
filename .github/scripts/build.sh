e8dcc0b1a46d86209f361c9eb9a69eb0ddae0e66
Jae Bradley <jae.b.bradley@gmail.com>
2022-07-02 13:33:11 -0400

    Update build

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,5 +5,5 @@
 chmod +x "$HOME/.bashrc"
 rm -f "$HOME/.bashrc" || fail "Could not delete bashrc file"
 
-. "$(dirname "${BASH_SOURCE}")/../../build.sh"
+. "$(dirname "${BASH_SOURCE}")/../../build.sh" "$(dirname "${BASH_SOURCE}")/../../"
 
