5de25212810389bdcd09ab054a6c7e468eeede38
Jae Bradley <jae.b.bradley@gmail.com>
2022-07-03 14:52:23 -0400

    Clone not checkout

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ main() {
 
   which git || fail "git is not installed"
 
-  git checkout https://github.com/jaebradley/bash-dependency-manager.git
+  git clone https://github.com/jaebradley/bash-dependency-manager.git
 
   bash bash-dependency-manager/install.sh "${installation_directory_path}"
 }
