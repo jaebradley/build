35f88e2b0f988ed4e3d7d955b9912cef8afe7f24
Jae Bradley <jae.b.bradley@gmail.com>
2022-07-03 14:40:24 -0400

    Update checkout source

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ main() {
 
   which git || fail "git is not installed"
 
-  git checkout git@github.com:jaebradley/bash-dependency-manager.git
+  git checkout https://github.com/jaebradley/bash-dependency-manager.git
 
   bash bash-dependency-manager/install.sh "${installation_directory_path}"
 }
