bf1d18a507faced92afc3599c3c643cddc18a374
Jae Bradley <jae.b.bradley@gmail.com>
2022-07-12 16:29:07 -0400

    Update

--- dependencies/Pycharm CE/install.sh
+++ dependencies/Pycharm CE/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-. "$(dirname ${BASH_SOURCE})/../../utilities/fail.sh"
-. "$(dirname ${BASH_SOURCE})/../../utilities/applications/install_application_from_disk_image.sh"
+. ""$(dirname "${BASH_SOURCE}")"/../../utilities/fail.sh"
+. ""$(dirname "${BASH_SOURCE}")"/../../utilities/applications/install_application_from_disk_image.sh"
 
 install() {
   if [[ "3" != "$#" ]]; then fail "Expected three arguments: a source binary URL, a target destination, and the dependency path"; fi
