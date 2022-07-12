f373aa365c8734ac77e71e55cfe34258dd060b4c
Jae Bradley <jae.b.bradley@gmail.com>
2022-07-12 16:23:04 -0400

    Update

--- dependencies/Intellij IDEA CE/install.sh
+++ dependencies/Intellij IDEA CE/install.sh
@@ -1,8 +1,7 @@
-
 #!/bin/bash
 
-. "$(dirname ${BASH_SOURCE})/../../utilities/fail.sh"
-. "$(dirname ${BASH_SOURCE})/../../utilities/applications/install_application_from_disk_image.sh"
+. "$(dirname "${BASH_SOURCE}")/../../utilities/fail.sh"
+. "$(dirname "${BASH_SOURCE}")/../../utilities/applications/install_application_from_disk_image.sh"
 
 install() {
   if [[ "3" != "$#" ]]; then fail "Expected three arguments: a source binary URL, a target destination, and the dependency path"; fi
@@ -10,7 +9,8 @@ install() {
   local -r source="$1"
   local -r name="Intellij IDEA CE.app"
 
-  install_application_from_disk_image "${source}" "${name}"  || fail "Failed to install ${name}"
+  install_application_from_disk_image "${source}" "${name}" 
+  if [[ "0" != "$?" ]]; then fail "Failed to install ${name}"; fi
 }
 
 install "$@"
