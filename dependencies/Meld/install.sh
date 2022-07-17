d2c4f6a1d601566467b5b7f247b3b5d177651f2e
Jae Bradley <jae.b.bradley@gmail.com>
2022-07-17 12:44:54 -0400

    Remove unnecessary validation scripts

--- dependencies/Meld/install.sh
+++ dependencies/Meld/install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-. "$(dirname "${BASH_SOURCE}")/../../utilities/fail.sh"
-. "$(dirname "${BASH_SOURCE}")/../../utilities/applications/install_application_from_disk_image.sh"
+directory_path="$(dirname "${BASH_SOURCE}")"
+if [[ "0" != "$?" ]]; then echo "Could not calculate directory path for ${BASH_SOURCE}" && exit 255; fi
+
+. "${directory_path}/../../utilities/fail.sh"
+if [[ "0" != "$?" ]]; then echo "Could not import utility on ${LINENO}" && exit 255; fi
+
+. "${directory_path}/../../utilities/applications/install_application_from_disk_image.sh"
+if [[ "0" != "$?" ]]; then fail "Could not import utility on ${LINENO}"; fi
 
 install() {
   if [[ "3" != "$#" ]]; then fail "Expected three arguments: a source binary URL, a target destination, and the dependency path"; fi
