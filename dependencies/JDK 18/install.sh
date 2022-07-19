3d2befd84634a88fda1d1361da263a6aa668755b
Jae Bradley <jae.b.bradley@gmail.com>
2022-07-18 21:42:24 -0400

    logging

--- dependencies/JDK 18/install.sh
+++ dependencies/JDK 18/install.sh
@@ -33,6 +33,9 @@ install() {
   mkdir -p "${mount_directory_path}" || fail "Error on line ${LINENO}"
   hdiutil attach "${image_path}" -mountpoint "${mount_directory_path}" -readonly -nokernel -verify -noignorebadchecksums -noautoopen || fail "Failed to attach ${source} image"
 
+  printf "Mount directory contents"
+  ls -la "${mount_directory_path}"
+
   local -r expected_application_path="${mount_directory_path}/JDK 18.0.1.1.pkg"
   if [[ ! -d "${expected_application_path}" ]]; then fail "Expected application ${expected_application_path} is not a directory"; fi
   if [[ ! -x "${expected_application_path}" ]]; then fail "Expected application ${expected_application_path} is not executable"; fi
