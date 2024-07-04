d6f5252bab15ce8da1a01350e74701ee17bb6c21
Jae Bradley <jae.b.bradley@gmail.com>
2024-07-03 22:26:54 -0400

    Update verify application existence

--- tests/utilities/verify_application_existence.sh
+++ tests/utilities/verify_application_existence.sh
@@ -2,7 +2,7 @@
 
 . "$(dirname ${BASH_SOURCE})/../utilities/fail.sh"
 
-function main() {
+function verify_application_existence() {
   if [[ $# -ne 1 ]]; then fail "Expected one argument, the name of the application"; fi
 
   local application_name="$1"
@@ -11,5 +11,3 @@ function main() {
   if [[ ! -d "/Applications/${application_name}" ]]; then fail "${application_name} does not exist"; fi
   if [[ ! -x "/Applications/${application_name}" ]]; then fail "${application_name} is not executable"; fi
 }
-
-main "$@"
