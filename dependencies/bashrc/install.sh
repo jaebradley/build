58b3b8b6a7e7b162ad4705964519f9be1bde02ab
Jae Bradley <jae.b.bradley@gmail.com>
2024-07-09 01:06:39 -0400

    Update

--- dependencies/bashrc/install.sh
+++ dependencies/bashrc/install.sh
@@ -10,9 +10,10 @@ function main() {
   local directory_path="$(dirname "${location_path}")"
   local file_name="$(basename "${location_path}")"
 
-  mkdir -p "${directory_path}" || fail "Failed to create directory ${directory_path}"
-  cp -n "${content_path}" "${location_path}" || fail "Failed to copy content from ${content_path} to ${location_path}"
-  chmod 755 "${location_path}" || fail "Unable to set permissions for "${location_path}""
+  # TODO: temporarily comment this out before thinking about how to install content
+  # mkdir -p "${directory_path}" || fail "Failed to create directory ${directory_path}"
+  # cp -n "${content_path}" "${location_path}" || fail "Failed to copy content from ${content_path} to ${location_path}"
+  # chmod 755 "${location_path}" || fail "Unable to set permissions for "${location_path}""
 }
 
 main "$@"
